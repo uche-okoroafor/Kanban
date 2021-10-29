710868610a4dfbf7b612c03eb1171cd9cbb9ee07
Erasmus Antwi <refugedesigns17@gmail.com>
2021-10-29 11:37:01 +0000

    Issue #16

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -102,7 +102,7 @@ exports.demoLogin = asyncHandler(async (req, res, next) => {
   }
 
 
-  const token = generateToken(145464654)
+  const token = generateToken(user._id)
   const secondsInWeek = 604800;
 
    res.cookie("token", token, {
