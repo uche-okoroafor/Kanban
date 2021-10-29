9175376ff67c1f2af482c15d21e5f91d65d6821e
Uche Okoroafor <ucheokoroafor018@gmail.com>
2021-10-29 15:09:23 +0400

    Update board.js

--- server/routes/board.js
+++ server/routes/board.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const protect = require("../middleware/auth");
 const { createBoard, removeBoard } = require("../controllers/board");
 
-router.route("/create").post(createBoard);
+router.route("/create").post(protect, createBoard);
 router.route("/remove").post(protect, removeBoard);
 
 module.exports = router;
