0f83d55a55d3f95a80dd18fe8685cbef0100c157
Ankit Babber <ankitbabber@gmail.com>
2021-11-03 03:02:31 -0500

    commented out code that prevented testing Board component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,21 +4,44 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Signup from './pages/SignUp/SignUp';
 import Dashboard from './pages/Dashboard/Dashboard';
+import { AuthProvider } from './context/useAuthContext';
 import { SocketProvider } from './context/useSocketContext';
 import { SnackBarProvider } from './context/useSnackbarContext';
+
+/*
+// removed for develop-ankit branch, integrate later
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 import { useImmerReducer } from 'use-immer';
 import { authReducer, authState } from './state';
+*/
+
 import './App.css';
 
 function App(): JSX.Element {
-  const [state] = useImmerReducer(authReducer, authState);
+  // removed for develop-ankit branch, integrate later
+  //const [state] = useImmerReducer(authReducer, authState);
 
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
         <SnackBarProvider>
-          <SocketProvider>
+          <AuthProvider>
+              <SocketProvider>
+                <Switch>
+                  <Route exact path="/login" component={Login} />
+                  <Route exact path="/signup" component={Signup} />
+                  <Route exact path="/dashboard">
+                    <Dashboard />
+                  </Route>
+                  <Route path="*">
+                    <Redirect to="/login" />
+                  </Route>
+                </Switch>
+              </SocketProvider>
+            </AuthProvider>
+          {
+          // removed for develop-ankit branch, integrate later
+          /*<SocketProvider>
             <Switch>
               <ProtectedRoute exact path="/" token={state.token} component={Dashboard} />
               <Route path="/login" component={Login} />
@@ -27,7 +50,8 @@ function App(): JSX.Element {
                 <Redirect to="/login" />
               </Route>
             </Switch>
-          </SocketProvider>
+          </SocketProvider>*/
+          }
         </SnackBarProvider>
       </BrowserRouter>
     </MuiThemeProvider>
