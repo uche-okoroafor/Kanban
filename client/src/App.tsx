f41f1cbf67e7cfd621b3da55c2b501e68cd5e6de
uche-okoroafor <ucheokoroafo01@gmil.com>
2021-11-07 18:37:04 +0400

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,34 +4,33 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Signup from './pages/SignUp/SignUp';
 import Dashboard from './pages/Dashboard/Dashboard';
-import Calendar from './pages/Calendar/Calendar';
-import { AuthProvider } from './context/useAuthContext';
 import { SocketProvider } from './context/useSocketContext';
 import { SnackBarProvider } from './context/useSnackbarContext';
-
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
+import Calendar from './pages/Calendar/Calendar';
+import { useImmerReducer } from 'use-immer';
+import { authReducer, authState } from './state';
 import './App.css';
 
 function App(): JSX.Element {
+  const [state] = useImmerReducer(authReducer, authState);
+
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
         <SnackBarProvider>
-          <AuthProvider>
-            <SocketProvider>
-              <Switch>
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/signup" component={Signup} />
-                <Route exact path="/calendar" component={Calendar} />
+          <SocketProvider>
+            <Switch>
+              <ProtectedRoute exact path="/" token={state.token} component={Dashboard} />
+              <Route path="/login" component={Login} />
+              <Route path="/calendar" component={Calendar} />
 
-                <Route exact path="/dashboard">
-                  <Dashboard />
-                </Route>
-                <Route path="*">
-                  <Redirect to="/login" />
-                </Route>
-              </Switch>
-            </SocketProvider>
-          </AuthProvider>
+              <Route path="/signup" component={Signup} />
+              <Route path="*">
+                <Redirect to="/login" />
+              </Route>
+            </Switch>
+          </SocketProvider>
         </SnackBarProvider>
       </BrowserRouter>
     </MuiThemeProvider>
