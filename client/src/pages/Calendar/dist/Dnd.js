26056a7956fc35476179341ef03e90ad33dba65c
Uche Okoroafor <ucheokoroafor018@gmail.com>
2021-11-08 19:23:11 +0400

    Delete client/src/pages/Calendar/dist directory

--- client/src/pages/Calendar/dist/Dnd.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-// import React, { useState } from 'react';
-// import events from './event';
-// import HTML5Backend from 'react-dnd-html5-backend';
-// import BigCalendar from 'react-big-calendar';
-// import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
-// import moment from 'moment';
-// import { Container } from '@material-ui/core';
-exports.__esModule = true;
