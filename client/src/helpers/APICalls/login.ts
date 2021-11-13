09a86d09cef4d4ac5b0ee8f7e33121384dc34c07
Erasmus Antwi <refugedesigns17@gmail.com>
2021-11-13 05:02:30 +0000

    fixed api key in sample.env

--- client/src/helpers/APICalls/login.ts
+++ client/src/helpers/APICalls/login.ts
@@ -20,7 +20,7 @@ const login = async (email: string, password: string): Promise<AuthApiData> => {
     body: JSON.stringify({ email, password }),
     credentials: 'include',
   };
-  return await fetch(`/auth/login`, fetchOptions)
+  return await fetch(`${process.env.REACT_APP_SERVER}/auth/login`, fetchOptions)
     .then((res) => res.json())
     .catch(() => ({
       error: { message: 'Unable to connect to server. Please try again' },
