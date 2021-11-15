63fc1051f4a249dc3c81f46a49357341fbc4984b
Ethan <ethanmoffat@hey.com>
2021-11-15 15:00:30 -0800

    uncomment auth code

--- client/src/context/useAuthContext.tsx
+++ client/src/context/useAuthContext.tsx
@@ -40,22 +40,22 @@ export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
       .catch((error) => console.error(error));
   }, [history]);
 
-  // // use our cookies to check if we can login straight away
-  // useEffect(() => {
-  //   const checkLoginWithCookies = async () => {
-  //     await loginWithCookies().then((data: AuthApiData) => {
-  //       if (data.success) {
-  //         updateLoginContext(data.success);
-  //         history.push('/dashboard');
-  //       } else {
-  //         // don't need to provide error feedback as this just means user doesn't have saved cookies or the cookies have not been authenticated on the backend
-  //         setLoggedInUser(null);
-  //         history.push('/login');
-  //       }
-  //     });
-  //   };
-  //   checkLoginWithCookies();
-  // }, [updateLoginContext, history]);
+  // use our cookies to check if we can login straight away
+  useEffect(() => {
+    const checkLoginWithCookies = async () => {
+      await loginWithCookies().then((data: AuthApiData) => {
+        if (data.success) {
+          updateLoginContext(data.success);
+          history.push('/dashboard');
+        } else {
+          // don't need to provide error feedback as this just means user doesn't have saved cookies or the cookies have not been authenticated on the backend
+          setLoggedInUser(null);
+          history.push('/login');
+        }
+      });
+    };
+    checkLoginWithCookies();
+  }, [updateLoginContext, history]);
 
   return <AuthContext.Provider value={{ loggedInUser, updateLoginContext, logout }}>{children}</AuthContext.Provider>;
 };
