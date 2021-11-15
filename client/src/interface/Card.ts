2767071cceafb87b6ca42a6ceb42a2245a2eecdf
ethan <ethanmoffat34@gmail.com>
2021-11-15 14:59:43 -0800

    Revert "added board"

--- client/src/interface/Card.ts
+++
@@ -1,9 +0,0 @@
-export interface Card {
-  id: string;
-  columnId: string;
-  name: string;
-  tag?: string;
-  dueDate?: Date;
-  description?: string;
-  createdAt?: Date;
-}
--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,12 @@
-import { Box } from '@material-ui/core';
-import Board from '../../components/Kanban/Board';
+import { Box, Typography } from '@material-ui/core';
+import React from 'react';
 
 export default function Dashboard(): JSX.Element {
   return (
-    <Box m={2}>
-      <Board />
-    </Box>
+    <>
+      <Box>
+        <Typography variant="h1">Dashboard Screen</Typography>
+      </Box>
+    </>
   );
 }
