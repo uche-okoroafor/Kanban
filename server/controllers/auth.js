818fac90a4a7c3a8d38ff165ad006bbc8ed0158f
uche-okoroafor <ucheokoroafo01@gmil.com>
2021-12-09 07:38:43 +0400

    updated

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,126 +1,126 @@
-const User = require("../models/User");
-const asyncHandler = require("express-async-handler");
-const generateToken = require("../utils/generateToken");
+const User = require('../models/User')
+const asyncHandler = require('express-async-handler')
+const generateToken = require('../utils/generateToken')
 
 // @route POST /auth/register
 // @desc Register user
 // @access Public
 exports.registerUser = asyncHandler(async (req, res, next) => {
-  const { username, email, password } = req.body;
+  const { username, email, password } = req.body
 
-  const emailExists = await User.findOne({ email });
+  const emailExists = await User.findOne({ email })
 
   if (emailExists) {
-    res.status(400);
-    throw new Error("A user with that email already exists");
+    res.status(400)
+    throw new Error('A user with that email already exists')
   }
 
-  const usernameExists = await User.findOne({ username });
+  const usernameExists = await User.findOne({ username })
 
   if (usernameExists) {
-    res.status(400);
-    throw new Error("A user with that username already exists");
+    res.status(400)
+    throw new Error('A user with that username already exists')
   }
 
   const user = await User.create({
     username,
     email,
-    password,
-  });
+    password
+  })
 
   if (user) {
-    const token = generateToken(user._id);
-    const secondsInWeek = 604800;
+    const token = generateToken(user._id)
+    const secondsInWeek = 604800
 
-    res.cookie("token", token, {
+    res.cookie('token', token, {
       httpOnly: true,
-      maxAge: secondsInWeek * 1000,
-    });
+      maxAge: secondsInWeek * 1000
+    })
 
     res.status(201).json({
       success: {
         user: {
           id: user._id,
           username: user.username,
-          email: user.email,
-        },
-      },
-    });
+          email: user.email
+        }
+      }
+    })
   } else {
-    res.status(400);
-    throw new Error("Invalid user data");
+    res.status(400)
+    throw new Error('Invalid user data')
   }
-});
+})
 
 // @route POST /auth/login
 // @desc Login user
 // @access Public
 exports.loginUser = asyncHandler(async (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body
 
-  const user = await User.findOne({ email });
+  const user = await User.findOne({ email })
 
   if (user && (await user.matchPassword(password))) {
-    const token = generateToken(user._id);
-    const secondsInWeek = 604800;
+    const token = generateToken(user._id)
+    const secondsInWeek = 604800
 
-    res.cookie("token", token, {
+    res.cookie('token', token, {
       httpOnly: true,
-      maxAge: secondsInWeek * 1000,
-    });
+      maxAge: secondsInWeek * 1000
+    })
 
     res.status(200).json({
       success: {
         user: {
           id: user._id,
           username: user.username,
-          email: user.email,
-        },
-      },
-    });
+          email: user.email
+        }
+      }
+    })
   } else {
-    res.status(401);
-    throw new Error("Invalid email or password");
+    res.status(401)
+    throw new Error('Invalid email or password')
   }
-});
+})
 
 // @route POST /auth/demo-login
 // @desc Login demo user
 // @access Public
 exports.demoLogin = asyncHandler(async (req, res, next) => {
-  const user = await User.findOne({ email: "demo@kanban.com" });
+  const user = await User.findOne({ email: 'demo@kanban.com' })
 
   if (!user) {
-    res.sendStatus(404);
+    res.sendStatus(404)
   }
 
-  const token = generateToken(145464654);
-  const secondsInWeek = 604800;
+  const token = generateToken(145464654)
+  const secondsInWeek = 604800
 
-  res.cookie("token", token, {
+  res.cookie('token', token, {
     httpOnly: true,
-    maxAge: secondsInWeek * 1000,
-  });
+    maxAge: secondsInWeek * 1000
+  })
 
   res.status(201).json({
     success: {
       user: {
         id: user._id,
         username: user.username,
-        email: user.email,
-      },
-    },
-  });
-});
+        email: user.email
+      }
+    }
+  })
+})
 
 // @route GET /auth/user
 // @desc Get user data with valid token
 // @access Private
 exports.loadUser = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id);
+  const user = await User.findById(req.user.id)
   if (!user) {
-    res.status(401);
-    throw new Error("Not authorized");
+    res.status(401)
+    throw new Error('Not authorized')
   }
 
   res.status(200).json({
@@ -128,17 +128,17 @@ exports.loadUser = asyncHandler(async (req, res, next) => {
       user: {
         id: user._id,
         username: user.username,
-        email: user.email,
-      },
-    },
-  });
-});
+        email: user.email
+      }
+    }
+  })
+})
 
 // @route GET /auth/logout
 // @desc Logout user
 // @access Public
 exports.logoutUser = asyncHandler(async (req, res, next) => {
-  res.clearCookie("token");
+  res.clearCookie('token')
 
-  res.send("You have successfully logged out");
-});
+  res.send('You have successfully logged out')
+})
