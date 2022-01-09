08a3b671ae71b3280efb22f2fe4ff87d4b9c3849
uche-okoroafor <ucheokoroafo01@gmil.com>
2022-01-09 23:10:41 +0400

    Add delete column

--- client/src/components/Kanban/Column/Column.tsx
+++ client/src/components/Kanban/Column/Column.tsx
@@ -1,14 +1,26 @@
-import { Box, Grid, Typography } from '@material-ui/core';
+import { Box, Grid, Typography, IconButton } from '@material-ui/core';
 import { Draggable, Droppable } from 'react-beautiful-dnd';
 import { Card } from '../../../interface/Card';
 import { Column } from '../../../interface/Column';
 import CardComponent from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
 import useStyles from './useStyles';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Dialog from '../../../components/Dialog/Dialog';
+import { useState, useCallback } from 'react';
 
 type ColumnProps = Column & { index: number };
 const ColumnComponent = ({ _id, columnTitle, cards, index }: ColumnProps): JSX.Element => {
   const classes = useStyles();
+  const [openDialog, setOpenDialog] = useState(false);
+
+  const handleOpenDialog = useCallback(() => {
+    setOpenDialog(true);
+  }, [setOpenDialog]);
+
+  const handleDelete = useCallback(() => {
+    setOpenDialog(false);
+  }, [setOpenDialog]);
 
   return (
     <Draggable draggableId={_id} index={index}>
@@ -28,6 +40,13 @@ const ColumnComponent = ({ _id, columnTitle, cards, index }: ColumnProps): JSX.E
                 <Typography className={classes.typography} variant="h5">
                   {columnTitle}
                 </Typography>
+                <IconButton
+                  onClick={handleOpenDialog}
+                  style={{ position: 'absolute', width: '0.5rem', height: '0.5rem', right: '4%', top: '10%' }}
+                >
+                  {' '}
+                  <DeleteIcon />
+                </IconButton>
               </Box>
               <Droppable droppableId={_id} type="card">
                 {(provided) => {
@@ -48,6 +67,13 @@ const ColumnComponent = ({ _id, columnTitle, cards, index }: ColumnProps): JSX.E
                       })}
                       {provided.placeholder}
                       <CardForm columnId={_id} />
+                      <Dialog
+                        openDialog={openDialog}
+                        setOpenDialog={setOpenDialog}
+                        item={'column'}
+                        handleDelete={handleDelete}
+                        title={columnTitle}
+                      />
                     </Grid>
                   );
                 }}
